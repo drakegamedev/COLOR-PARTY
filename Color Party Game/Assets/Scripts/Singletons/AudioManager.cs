3ee8054feac7dba75d5444c3239ebf382e7fd8c9
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-07-28 00:27:28 +0800

    Polished AudioManager Variables

--- Color Party Game/Assets/Scripts/Singletons/AudioManager.cs
+++ Color Party Game/Assets/Scripts/Singletons/AudioManager.cs
@@ -14,11 +14,11 @@ public class AudioManager : MonoBehaviour
     }
 
     [Header("References")]
-    public AudioDataCollection[] AudioCollections;
+    [SerializeField] private AudioDataCollection[] audioCollections;                            // Audio Collection Array
 
     [Space]
-    public GameObject MusicHolder;
-    public GameObject SoundHolder;
+    [SerializeField] private GameObject musicHolder;                                            // BGM Holder Reference
+    [SerializeField] private GameObject soundHolder;                                            // SFX Holder Reference
 
     #region Singleton
     void Awake()
@@ -30,21 +30,22 @@ public class AudioManager : MonoBehaviour
         else
         {
             Destroy(this.gameObject);
+            return;
         }
 
         // Initialize Audio Collections
-        foreach (AudioDataCollection collectionData in AudioCollections)
+        foreach (AudioDataCollection collectionData in audioCollections)
         {
             foreach (AudioData audioData in collectionData.Audios)
             {
                 switch (audioData.GetAudioType())
                 {
                     case AudioData.AudioType.BGM:
-                        audioData.Initialize(MusicHolder);
+                        audioData.Initialize(musicHolder);
                         break;
 
                     case AudioData.AudioType.SFX:
-                        audioData.Initialize(SoundHolder);
+                        audioData.Initialize(soundHolder);
                         break;
                 };
             }
@@ -53,11 +54,14 @@ public class AudioManager : MonoBehaviour
     #endregion
 
     #region Audio Methods
-    // Play Audio
+    /// <summary>
+    /// Play Audio
+    /// </summary>
+    /// <param name="id"></param>
     public void Play(string id)
     {
         // Find Audio in Audio Collections
-        foreach (AudioDataCollection collectionData in AudioCollections)
+        foreach (AudioDataCollection collectionData in audioCollections)
         {
             // Check within AudioData Types
             foreach (AudioData audioData in collectionData.Audios)
@@ -76,11 +80,14 @@ public class AudioManager : MonoBehaviour
         return;
     }
 
-    // Stop Audio
+    /// <summary>
+    /// Stop Audio
+    /// </summary>
+    /// <param name="id"></param>
     public void Stop(string id)
     {
         // Find Audio in Audio Collections
-        foreach (AudioDataCollection collectionData in AudioCollections)
+        foreach (AudioDataCollection collectionData in audioCollections)
         {
             // Check within AudioData Types
             foreach (AudioData audioData in collectionData.Audios)
@@ -99,11 +106,15 @@ public class AudioManager : MonoBehaviour
         return;
     }
 
-    // Modify Audio Pitch
+    /// <summary>
+    /// Modify Audio Pitch
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="amount"></param>
     public void ModifyPitch(string id, float amount)
     {
         // Find Audio in Audio Collections
-        foreach (AudioDataCollection collectionData in AudioCollections)
+        foreach (AudioDataCollection collectionData in audioCollections)
         {
             // Check within AudioData Types
             foreach (AudioData audioData in collectionData.Audios)
